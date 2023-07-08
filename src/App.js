54b3245dc0c064f161a4ec212c28abca448590c7
charles-okunzo <okunzopeter01@gmail.com>
2023-07-08 18:28:53 +0300

    Use case of Fragments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import FragmentComponent from "./components/FragmentComponent";
+import Table from "./components/Table";
 
 function App() {
   return (
     <div className="App">
       <FragmentComponent />
+      <Table />
     </div>
   );
 }
---
+++ src/components/Column.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Column(){
+    return (
+        <React.Fragment>
+            <td>Hello</td>
+            <td>there!</td>
+        </React.Fragment>
+    )
+}
